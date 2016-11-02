ef40b0788c5c5934ca3082088661982e34f3866b
jusjusjus <jschwabedal@gmail.com>
2016-11-02 23:39:40 +0100

    Cost is euclidian distance.

--- js/main.js
+++ js/main.js
@@ -20,18 +20,19 @@ function render(s1,s2,s3,s4,s5)
 	var datalist = [];
 
 	globaldata.features.forEach(function(feature) {
-		var stadtteilfactor = feature.properties.dichte * s1
-				    + feature.properties.farbe * s2
-				    + feature.properties.wahl * s3;
+		var distance = Math.abs(feature.properties.dichte - s1) 
+			     + Math.abs(feature.properties.farbe - s2)
+			     + Math.abs(feature.properties.wahl - s3);
+		var stadtteilfactor = 2/(1+Math.exp(distance));
 
-		stadtteilfactor = (stadtteilfactor > 0) * stadtteilfactor; //ReLU
-
-		// Offset von 0.3 soll durch 'Wohngebiete' als neue Dimension ersetzt werden.
-		var gewicht =  3. * stadtteilfactor * (0.3 + feature.properties.stops * s5 - feature.properties.kinder * s4);
-
-		if(gewicht > 0.01) {					   // ReLU
-			var coordinates = feature.geometry.coordinates;
-			datalist.push([coordinates[1], coordinates[0], gewicht])
+		if(stadtteilfactor > 0.05)
+		{
+			var gewicht =  stadtteilfactor * (feature.properties.wohnung * 0.1 + feature.properties.stops * s5 - feature.properties.kinder * s4);
+			if(gewicht > 0.01)
+			{					   // ReLU
+				var coordinates = feature.geometry.coordinates;
+				datalist.push([coordinates[1], coordinates[0], gewicht])
+			}
 		}
 	});
 	heatlayer = L.heatLayer(datalist, {radius: 50}).addTo(map);
@@ -53,6 +54,12 @@ function renderObj()
 	
 	var val5 = $('#slider5').val();
 	val5 = (val5 / 10);
+
+	console.log(val1);
+	console.log(val2);
+	console.log(val3);
+	console.log(val4);
+	console.log(val5);
 
 	render(val1,val2,val3,val4,val5);
 }
