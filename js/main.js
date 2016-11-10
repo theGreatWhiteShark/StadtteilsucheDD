46c17a8e430f46f71a512ba8753af6ffba228221
Justus Schwabedal <jschwabedal@gmail.com>
2016-11-10 13:37:34 +0100

    Disable/enable all sliders.

--- js/main.js
+++ js/main.js
@@ -51,33 +51,74 @@ function render(district_props,s4,s5)
 }
 
 
+function disableSlider(slider_id)
+{
+	$(slider_id).prop('disabled',  true);
+}
+
+
+function enableSlider(slider_id)
+{
+	$(slider_id).prop('disabled',  false);
+}
+
 
 function renderObj()
 {
 	var district_props = [];
-	if( $('#cb_dichte:checked').val() ) {
+	if( $('#cb_dichte:checked').val() )
+	{
+		enableSlider('#slider1');
 		var val1 = $('#slider1').val();
 		val1 = (val1 / 5) - 1;
 		district_props.push([val1, 0]);
 	}
+	else
+	{
+		$('#slider1').prop('disabled', true);
+	}
 
-	if( $('#cb_farbe:checked').val() ) {
+	if( $('#cb_farbe:checked').val() )
+	{
+		enableSlider('#slider2');
 		var val2 = $('#slider2').val();
 		val2 = (val2 / 5) - 1;
 		district_props.push([val2, 1]);
 	}
+	else
+	{
+		$('#slider2').prop('disabled', true);
+	}
 
-	if( $('#cb_wahl:checked').val() ) {
+	if( $('#cb_wahl:checked').val() )
+	{
+		enableSlider('#slider3');
 		var val3 = $('#slider3').val();
 		val3 = (val3 / 5) - 1;
 		district_props.push([val3, 2]);
 	}
+	else
+	{
+		$('#slider3').prop('disabled', true);
+	}
 
+	if( district_props.length == 0 )
+	{
+		disableSlider('#slider4');
+		disableSlider('#slider5');
+	}
+	else
+	{
+		enableSlider('#slider4');
+		enableSlider('#slider5');
+	}
+
+	
 	var val4 = $('#slider4').val();
 	val4 = (val4 / 5) - 1;
 	
 	var val5 = $('#slider5').val();
 	val5 = (val5 / 10);
 
-	render(district_props,val4,val5);
+	render(district_props, val4, val5);
 }
